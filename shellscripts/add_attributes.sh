a670af0da1b8441994f50d904ec190a15b8b3003
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-01-26 14:06:45 +0100

    adding add attributes script to workflow; updating xmls

--- shellscripts/add_attributes.sh
+++ shellscripts/add_attributes.sh
@@ -1,4 +1,5 @@
 # bin/bash
 
 echo "add attributes"
-add-attributes -g "./data/indexes/*.xml" -b "https://amp.acdh.oeaw.ac.at"
+add-attributes -g "./data/indexes/*.xml" -b "https://id.acdh.oeaw.ac.at"
+add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at"
